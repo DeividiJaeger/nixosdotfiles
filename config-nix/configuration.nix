2d195715b4ad9ee04dc4ad6ce5a41295bb8a8ef7
DeividiJaeger <deividi.coontato@gmail.com>
2025-02-10 11:02:01 -0300

    edição da cor da janela ativa e autoexec no brave

--- config-nix/configuration.nix
+++ config-nix/configuration.nix
@@ -17,7 +17,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "deivi-nixos"; # Define your hostname.
   #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
@@ -208,6 +208,6 @@ programs.hyprland.enable = true;
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
+  system.stateVersion = "24.11"; # Did you read the comment?
 
 }
